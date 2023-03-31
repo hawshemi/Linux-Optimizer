aeb6b0197377603e3a3f0e780047aa48269ec15e
Hawshemi <rhawshemi@gmail.com>
2023-03-31 10:48:39 +0330

    Script Update.
    Script Update.

--- ubuntu-optimizer.sh
+++ ubuntu-optimizer.sh
@@ -7,8 +7,8 @@ echo "This script will automatically Optimize the Server."
 echo "Root access is required." 
 echo "Source is @ https://github.com/hawshemi/" 
 echo 
-
 sleep 2
+
 
 # Update & Upgrade & Remove & Clean
 sudo apt update 
@@ -22,15 +22,19 @@ sleep 1
 sudo apt -y autoclean
 sleep 1
 
+
 # Install useful packages
 sudo apt -y install software-properties-common ufw wget snap snapd jq qrencode curl iptables lsb-release ca-certificates ubuntu-keyring gnupg2 preload locales nano apt-utils git socat cron busybox bash-completion
 sleep 0.5
+
 
 # Snap Install
 sudo snap install core
 sudo snap refresh core
 sleep 1
 
+
+## SWAP
 # 2 GB Swap Size
 SWAP_SIZE=2G
 
@@ -43,39 +47,45 @@ sudo chmod 600 $SWAP_PATH                # Set proper permission
 sudo mkswap $SWAP_PATH                   # Setup swap         
 sudo swapon $SWAP_PATH                   # Enable swap
 echo "$SWAP_PATH   none    swap    sw    0   0" | sudo tee -a /etc/fstab # Add to fstab
-
 sleep 1
 
+
+## SYSCTL Optimize
+# Paths
 SYS_PATH="/etc/sysctl.conf"
-UFW_PATH="/etc/ufw/sysctl.conf"
+UFW_PATH="/etc/default/ufw"
 LIM_PATH="/etc/security/limits.conf"
 
 # Optimize Swap Settings
 echo 'vm.swappiness=10' | tee -a $SYS_PATH
 echo 'vm.vfs_cache_pressure=50' | tee -a $SYS_PATH
-
 sleep 0.5
 
 # Optimize Network Settings
-echo 'fs.file-max = 51200' | tee -a $SYS_PATH | tee -a $UFW_PATH
+echo 'fs.file-max = 51200' | tee -a $SYS_PATH
 
-echo 'net.core.rmem_default = 1048576' | tee -a $SYS_PATH | tee -a $UFW_PATH
-echo 'net.core.rmem_max = 2097152' | tee -a $SYS_PATH | tee -a $UFW_PATH
-echo 'net.core.wmem_default = 1048576' | tee -a $SYS_PATH | tee -a $UFW_PATH
-echo 'net.core.wmem_max = 2097152' | tee -a $SYS_PATH | tee -a $UFW_PATH
+echo 'net.core.rmem_default = 1048576' | tee -a $SYS_PATH
+echo 'net.core.rmem_max = 2097152' | tee -a $SYS_PATH
+echo 'net.core.wmem_default = 1048576' | tee -a $SYS_PATH
+echo 'net.core.wmem_max = 2097152' | tee -a $SYS_PATH
 
-echo 'net.core.netdev_max_backlog = 250000' | tee -a $SYS_PATH | tee -a $UFW_PATH
-echo 'net.core.somaxconn = 3000' | tee -a $SYS_PATH | tee -a $UFW_PATH
+echo 'net.core.netdev_max_backlog = 250000' | tee -a $SYS_PATH
+echo 'net.core.somaxconn = 3000' | tee -a $SYS_PATH
 
-echo 'net.ipv4.tcp_fastopen = 3' | tee -a $SYS_PATH | tee -a $UFW_PATH
+echo 'net.ipv4.tcp_fastopen = 3' | tee -a $SYS_PATH
 
-echo 'net.ipv4.tcp_mtu_probing = 1' | tee -a $SYS_PATH | tee -a $UFW_PATH
+echo 'net.ipv4.tcp_mtu_probing = 1' | tee -a $SYS_PATH
 
-echo 'net.core.default_qdisc = fq' | tee -a $SYS_PATH | tee -a $UFW_PATH
-echo 'net.ipv4.tcp_congestion_control = bbr' | tee -a $SYS_PATH | tee -a $UFW_PATH
+echo 'net.core.default_qdisc = fq' | tee -a $SYS_PATH
+echo 'net.ipv4.tcp_congestion_control = bbr' | tee -a $SYS_PATH
 
+
+# Changing the UFW config to use System config.
+sed -i 's+/etc/ufw/sysctl.conf+/etc/sysctl.conf+gI' /etc/default/ufw
 sleep 0.5
 
+
+# Optimize the Limits
 echo '* soft     nproc          655350' | tee -a $LIM_PATH
 echo '* hard     nproc          655350' | tee -a $LIM_PATH
 echo '* soft     nofile         655350' | tee -a $LIM_PATH
@@ -87,10 +97,10 @@ echo 'root soft     nofile         655350' | tee -a $LIM_PATH
 echo 'root hard     nofile         655350' | tee -a $LIM_PATH
 
 sudo sysctl -p
-
 sleep 1
 
-# Firewall Optimization
+
+# Firewall Allow
 sudo ufw allow 21
 sudo ufw allow 21/udp
 sudo ufw allow 22
@@ -100,8 +110,8 @@ sudo ufw allow 80/udp
 sudo ufw allow 443
 sudo ufw allow 443/udp
 
-# Outro
 
+# Outro
 echo 
 echo "Done! Server is Optimized."
 echo "Reboot in 5 seconds..."
