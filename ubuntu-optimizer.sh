7afc4275054ecc8a061e889f2e07da4c1fb28896
Hawshemi <rhawshemi@gmail.com>
2023-03-31 16:20:00 +0330

    Update.
    Update.

--- ubuntu-optimizer.sh
+++ ubuntu-optimizer.sh
@@ -3,7 +3,7 @@
 
 # Intro
 echo 
-echo "This script will automatically Optimize the Ubuntu Server."
+echo "This script will automatically Optimize your Ubuntu Server."
 echo "Root access is required." 
 echo "Source is @ https://github.com/hawshemi/" 
 echo 
@@ -19,111 +19,129 @@ check_if_running_as_root() {
   fi
 }
 
+
 # Detect if OS is Ubuntu
-ID= grep -ioP '^ID=\K.+' /etc/os-release
-if ! [[ $ID = "ubuntu" ]]; then
-    echo "Not Ubuntu"
-    exit 1
-fi
+detect_ubuntu() {
+  ID= grep -ioP '^ID=\K.+' /etc/os-release
+  if ! [[ $ID = "ubuntu" ]]; then
+      echo "Not Ubuntu"
+      exit 1
+  fi
+}
+
 
 # Update & Upgrade & Remove & Clean
-sudo apt update 
-sleep 0.5
-sudo apt -y upgrade
-sleep 0.5
-sudo apt -y dist-upgrade
-sleep 0.5
-sudo apt -y autoremove
-sleep 0.5
-sudo apt -y autoclean
-sleep 1
+complete_update() {
+  sudo apt update 
+  sleep 0.5
+  sudo apt -y upgrade
+  sleep 0.5
+  sudo apt -y dist-upgrade
+  sleep 0.5
+  sudo apt -y autoremove
+  sleep 0.5
+  sudo apt -y autoclean
+  sleep 1
+}
 
 
-# Install useful packages
-sudo apt -y install software-properties-common ufw wget snap snapd jq qrencode curl iptables lsb-release ca-certificates ubuntu-keyring gnupg2 preload locales nano apt-utils git socat cron busybox bash-completion
-sleep 0.5
+## Install useful packages
+installations() {
+  sudo apt -y install software-properties-common apt-transport-https snap snapd iptables lsb-release ca-certificates ubuntu-keyring gnupg2 apt-utils cron bash-completion
+  sudo apt -y install curl git unzip ufw wget preload locales nano vim python3 jq qrencode socat busybox net-tools
+  sleep 0.5
+
+  # Snap Install & Refresh
+  sudo snap install core
+  sudo snap refresh core
+  sleep 1
+}
 
 
-# Snap Install
-sudo snap install core
-sudo snap refresh core
-sleep 1
+## Swap Maker
+swap_maker() {
+  # 2 GB Swap Size
+  SWAP_SIZE=2G
+
+  # Default Swap Path
+  SWAP_PATH="/swapfile"
+
+  # Make Swap
+  sudo fallocate -l $SWAP_SIZE $SWAP_PATH  # Allocate size
+  sudo chmod 600 $SWAP_PATH                # Set proper permission
+  sudo mkswap $SWAP_PATH                   # Setup swap         
+  sudo swapon $SWAP_PATH                   # Enable swap
+  echo "$SWAP_PATH   none    swap    sw    0   0" | sudo tee -a /etc/fstab # Add to fstab
+  sleep 1
+}
+
+## SYSCTL Optimization
+sysctl_optimizations() {
+  # Paths
+  SYS_PATH="/etc/sysctl.conf"
+  LIM_PATH="/etc/security/limits.conf"
+
+  # Optimize Swap Settings
+  echo 'vm.swappiness=10' | tee -a $SYS_PATH
+  echo 'vm.vfs_cache_pressure=50' | tee -a $SYS_PATH
+  sleep 0.5
+
+  # Optimize Network Settings
+  echo 'fs.file-max = 51200' | tee -a $SYS_PATH
+
+  echo 'net.core.rmem_default = 1048576' | tee -a $SYS_PATH
+  echo 'net.core.rmem_max = 2097152' | tee -a $SYS_PATH
+  echo 'net.core.wmem_default = 1048576' | tee -a $SYS_PATH
+  echo 'net.core.wmem_max = 2097152' | tee -a $SYS_PATH
+
+  echo 'net.core.netdev_max_backlog = 250000' | tee -a $SYS_PATH
+  echo 'net.core.somaxconn = 3000' | tee -a $SYS_PATH
+
+  echo 'net.ipv4.tcp_fastopen = 3' | tee -a $SYS_PATH
+
+  echo 'net.ipv4.tcp_mtu_probing = 1' | tee -a $SYS_PATH
+
+  echo 'net.core.default_qdisc = fq' | tee -a $SYS_PATH
+  echo 'net.ipv4.tcp_congestion_control = bbr' | tee -a $SYS_PATH
+  sleep 0.5
+}
 
 
-## SWAP
-# 2 GB Swap Size
-SWAP_SIZE=2G
+## Firewall Optimizations
+firewall_optimizations() {
+  # Open default ports.
+  sudo ufw allow 21
+  sudo ufw allow 21/udp
+  sudo ufw allow 22
+  sudo ufw allow 22/udp
+  sudo ufw allow 80
+  sudo ufw allow 80/udp
+  sudo ufw allow 443
+  sudo ufw allow 443/udp
 
-# Default Swap Path
-SWAP_PATH="/swapfile"
-
-# Run
-sudo fallocate -l $SWAP_SIZE $SWAP_PATH  # Allocate size
-sudo chmod 600 $SWAP_PATH                # Set proper permission
-sudo mkswap $SWAP_PATH                   # Setup swap         
-sudo swapon $SWAP_PATH                   # Enable swap
-echo "$SWAP_PATH   none    swap    sw    0   0" | sudo tee -a /etc/fstab # Add to fstab
-sleep 1
-
-
-## SYSCTL Optimize
-# Paths
-SYS_PATH="/etc/sysctl.conf"
-LIM_PATH="/etc/security/limits.conf"
-
-# Optimize Swap Settings
-echo 'vm.swappiness=10' | tee -a $SYS_PATH
-echo 'vm.vfs_cache_pressure=50' | tee -a $SYS_PATH
-sleep 0.5
-
-# Optimize Network Settings
-echo 'fs.file-max = 51200' | tee -a $SYS_PATH
-
-echo 'net.core.rmem_default = 1048576' | tee -a $SYS_PATH
-echo 'net.core.rmem_max = 2097152' | tee -a $SYS_PATH
-echo 'net.core.wmem_default = 1048576' | tee -a $SYS_PATH
-echo 'net.core.wmem_max = 2097152' | tee -a $SYS_PATH
-
-echo 'net.core.netdev_max_backlog = 250000' | tee -a $SYS_PATH
-echo 'net.core.somaxconn = 3000' | tee -a $SYS_PATH
-
-echo 'net.ipv4.tcp_fastopen = 3' | tee -a $SYS_PATH
-
-echo 'net.ipv4.tcp_mtu_probing = 1' | tee -a $SYS_PATH
-
-echo 'net.core.default_qdisc = fq' | tee -a $SYS_PATH
-echo 'net.ipv4.tcp_congestion_control = bbr' | tee -a $SYS_PATH
+  # Change the UFW config to use System config.
+  sed -i 's+/etc/ufw/sysctl.conf+/etc/sysctl.conf+gI' /etc/default/ufw
+  sleep 0.5
+}
 
 
-# Changing the UFW config to use System config.
-sed -i 's+/etc/ufw/sysctl.conf+/etc/sysctl.conf+gI' /etc/default/ufw
-sleep 0.5
+# System Limits Optimizations
+limits_optimizations() {
+  echo '* soft     nproc          655350' | tee -a $LIM_PATH
+  echo '* hard     nproc          655350' | tee -a $LIM_PATH
+  echo '* soft     nofile         655350' | tee -a $LIM_PATH
+  echo '* hard     nofile         655350' | tee -a $LIM_PATH
 
+  echo 'root soft     nproc          655350' | tee -a $LIM_PATH
+  echo 'root hard     nproc          655350' | tee -a $LIM_PATH
+  echo 'root soft     nofile         655350' | tee -a $LIM_PATH
+  echo 'root hard     nofile         655350' | tee -a $LIM_PATH
 
-# Optimize the Limits
-echo '* soft     nproc          655350' | tee -a $LIM_PATH
-echo '* hard     nproc          655350' | tee -a $LIM_PATH
-echo '* soft     nofile         655350' | tee -a $LIM_PATH
-echo '* hard     nofile         655350' | tee -a $LIM_PATH
+  sudo sysctl -p
 
-echo 'root soft     nproc          655350' | tee -a $LIM_PATH
-echo 'root hard     nproc          655350' | tee -a $LIM_PATH
-echo 'root soft     nofile         655350' | tee -a $LIM_PATH
-echo 'root hard     nofile         655350' | tee -a $LIM_PATH
+  sleep 1
 
-sudo sysctl -p
-sleep 1
-
-
-# Firewall Allow
-sudo ufw allow 21
-sudo ufw allow 21/udp
-sudo ufw allow 22
-sudo ufw allow 22/udp
-sudo ufw allow 80
-sudo ufw allow 80/udp
-sudo ufw allow 443
-sudo ufw allow 443/udp
+}
 
 
 # Outro
