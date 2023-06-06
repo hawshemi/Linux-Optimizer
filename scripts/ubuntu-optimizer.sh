d7616d6a5636bc5181468d08cd2969946fda1006
Hawshemi <rhawshemi@gmail.com>
2023-06-06 16:53:35 +0330

    Update.
    Update.

--- scripts/ubuntu-optimizer.sh
+++ scripts/ubuntu-optimizer.sh
@@ -147,47 +147,44 @@ swap_maker() {
 # Remove Old SYSCTL Config to prevent duplicates.
 remove_old_sysctl() {
     sed -i '/fs.file-max/d' $SYS_PATH
-    sed -i '/fs.inotify.max_user_instances/d' $SYS_PATH
 
-    sed -i '/net.ipv4.tcp_window_scaling/d' $SYS_PATH
-    sed -i '/net.ipv4.tcp_fastopen/d' $SYS_PATH
+    # Swap Settings
+    sed -i 'vm.swappiness/d' $SYS_PATH
+    sed -i 'vm.vfs_cache_pressure/d' $SYS_PATH
+
+    # Network Settings
+    sed -i 'fs.file-max/d' $SYS_PATH
+
+    sed -i 'net.ipv4.tcp_window_scaling/d' $SYS_PATH
+    sed -i 'net.ipv4.tcp_fastopen/d' $SYS_PATH
     
-    sed -i '/net.ipv4.tcp_syncookies/d' $SYS_PATH
-    sed -i '/net.ipv4.tcp_fin_timeout/d' $SYS_PATH
-    sed -i '/net.ipv4.tcp_tw_reuse/d' $SYS_PATH
-    sed -i '/net.ipv4.ip_local_port_range/d' $SYS_PATH
-    sed -i '/net.ipv4.tcp_max_syn_backlog/d' $SYS_PATH
-    sed -i '/net.ipv4.tcp_max_tw_buckets/d' $SYS_PATH
-    sed -i '/net.ipv4.route.gc_timeout/d' $SYS_PATH
+    sed -i 'net.core.rmem_default/d' $SYS_PATH
+    sed -i 'net.core.rmem_max/d' $SYS_PATH
+    sed -i 'net.core.wmem_default/d' $SYS_PATH
+    sed -i 'net.core.wmem_max/d' $SYS_PATH
+    sed -i 'net.core.netdev_max_backlog/d' $SYS_PATH
+    sed -i 'net.core.somaxconn/d' $SYS_PATH
+    sed -i 'net.ipv4.tcp_mtu_probing/d' $SYS_PATH
 
-    sed -i '/net.ipv4.tcp_syn_retries/d' $SYS_PATH
-    sed -i '/net.ipv4.tcp_synack_retries/d' $SYS_PATH
-    sed -i '/net.core.somaxconn/d' $SYS_PATH
-    sed -i '/net.core.netdev_max_backlog/d' $SYS_PATH
-    sed -i '/net.ipv4.tcp_timestamps/d' $SYS_PATH
-    sed -i '/net.ipv4.tcp_max_orphans/d' $SYS_PATH
-    sed -i '/net.ipv4.ip_forward/d' $SYS_PATH
+    sed -i 'net.ipv4.tcp_retries2/d' $SYS_PATH
+    sed -i 'net.ipv4.tcp_slow_start_after_idle/d' $SYS_PATH
+    sed -i 'net.ipv4.ip_forward/d' $SYS_PATH
 
-    #IPv6
-    sed -i '/net.ipv6.conf.all.disable_ipv6/d' $SYS_PATH
-    sed -i '/net.ipv6.conf.default.disable_ipv6/d' $SYS_PATH
-    sed -i '/net.ipv6.conf.all.forwarding/d' $SYS_PATH
+    sed -i 'net.ipv6.conf.all.disable_ipv6/d' $SYS_PATH
+    sed -i 'net.ipv6.conf.default.disable_ipv6/d' $SYS_PATH
+    sed -i 'net.ipv6.conf.all.forwarding/d' $SYS_PATH
+
+    # BBR
+    sed -i 'net.core.default_qdisc/d' $SYS_PATH 
+    sed -i 'net.ipv4.tcp_congestion_control/d' $SYS_PATH
 
     # System Limits.
     sed -i '/soft/d' $LIM_PATH
     sed -i '/hard/d' $LIM_PATH
 
-    # BBR
-    sed -i '/net.core.default_qdisc/d' $SYS_PATH
-    sed -i '/net.ipv4.tcp_congestion_control/d' $SYS_PATH
-    sed -i '/net.ipv4.tcp_ecn/d' $SYS_PATH
-
     # uLimit
     sed -i '/1000000/d' $PROF_PATH
 
-    #SWAP
-    sed -i '/vm.swappiness/d' $SYS_PATH
-    sed -i '/vm.vfs_cache_pressure/d' $SYS_PATH
 }
 
 
@@ -207,19 +204,18 @@ sysctl_optimizations() {
 
     echo 'net.ipv4.tcp_window_scaling = 1' >> $SYS_PATH
     echo 'net.ipv4.tcp_fastopen = 3' >> $SYS_PATH
-
+    
     echo 'net.core.rmem_default = 1048576' >> $SYS_PATH
     echo 'net.core.rmem_max = 2097152' >> $SYS_PATH
     echo 'net.core.wmem_default = 1048576' >> $SYS_PATH
     echo 'net.core.wmem_max = 2097152' >> $SYS_PATH
     echo 'net.core.netdev_max_backlog = 16384' >> $SYS_PATH
     echo 'net.core.somaxconn = 32768' >> $SYS_PATH
-    echo 'net.ipv4.tcp_fastopen = 3' >> $SYS_PATH
     echo 'net.ipv4.tcp_mtu_probing = 1' >> $SYS_PATH
 
     echo 'net.ipv4.tcp_retries2 = 8' >> $SYS_PATH
     echo 'net.ipv4.tcp_slow_start_after_idle = 0' >> $SYS_PATH
-    echo 'net.ipv4.ip_forward = 1' | tee -a $SYS_PATH
+    echo 'net.ipv4.ip_forward = 1' >> $SYS_PATH
 
     echo 'net.ipv6.conf.all.disable_ipv6 = 0' >> $SYS_PATH
     echo 'net.ipv6.conf.default.disable_ipv6 = 0' >> $SYS_PATH
