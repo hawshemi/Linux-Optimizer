8dc19b4420628fe5d8f2429a56dd615255f0d383
Hawshemi <rhawshemi@gmail.com>
2023-06-06 15:34:04 +0330

    Update.
    Update.

--- scripts/centos-optimizer.sh
+++ scripts/centos-optimizer.sh
@@ -353,7 +353,7 @@ ufw_optimizations() {
     sed -i 's+/etc/ufw/sysctl.conf+/etc/sysctl.conf+gI' /etc/default/ufw
 
     # Enable & Reload
-    sudo ufw enable
+    echo "y" | sudo ufw enable
     sudo ufw reload
     echo 
     green_msg 'Firewall is Optimized.'
@@ -524,7 +524,6 @@ main() {
             ;;
         esac
     done
-
 }
 
 
