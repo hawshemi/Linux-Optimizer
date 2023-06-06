8b1766dd3fce84b3974b17bd0340d1c5b2a9822b
Hawshemi <rhawshemi@gmail.com>
2023-06-06 14:37:51 +0330

    Update.
    Update.

--- linux-optimizer.sh
+++ linux-optimizer.sh
@@ -29,7 +29,7 @@ echo
 green_msg '======================================================='
 green_msg 'This script will automatically Optimize your Linux Server.'
 green_msg 'Tested on: Ubuntu 16+, Debian 11+, CentOS 8+, Fedora 37+'
-yellow_msg 'Root access is required.' 
+green_msg 'Root access is required.' 
 green_msg 'Source is @ https://github.com/hawshemi/linux-optimizer' 
 green_msg '======================================================='
 echo 
