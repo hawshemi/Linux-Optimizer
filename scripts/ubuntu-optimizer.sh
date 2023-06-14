b449836a2071f8828d0aff7127ba53570a0c005b
Hawshemi <rhawshemi@gmail.com>
2023-06-14 18:32:07 +0330

    Update.
    Update.

--- scripts/ubuntu-optimizer.sh
+++ scripts/ubuntu-optimizer.sh
@@ -133,7 +133,7 @@ installations() {
 
 # Enable packages at server boot
 enable_packages() {
-    sudo systemctl enable preload haveged cron
+    sudo systemctl enable preload haveged cron nftables
     echo 
     green_msg 'Packages Enabled Succesfully.'
     echo
