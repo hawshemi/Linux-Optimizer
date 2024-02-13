3db8c47eccd40f768421bfe65e639bfe087a28f9
Hawshemi <rhawshemi@gmail.com>
2024-02-13 23:00:52 +0330

    Update ubuntu-optimizer.sh
    Better `SYSCTL` Optimizations.

--- scripts/ubuntu-optimizer.sh
+++ scripts/ubuntu-optimizer.sh
@@ -283,98 +283,228 @@ sysctl_optimizations() {
         -e '/net.ipv4.tcp_dsack/d' \
         -e '/net.ipv4.tcp_slow_start_after_idle/d' \
         -e '/net.ipv4.tcp_window_scaling/d' \
+        -e '/net.ipv4.tcp_adv_win_scale/d' \
         -e '/net.ipv4.tcp_ecn/d' \
-        -e '/net.ipv4.ip_forward/d' \
+        -e '/net.ipv4.tcp_ecn_fallback/d' \
+        -e '/net.ipv4.tcp_syncookies/d' \
         -e '/net.ipv4.udp_mem/d' \
         -e '/net.ipv6.conf.all.disable_ipv6/d' \
-        -e '/net.ipv6.conf.all.forwarding/d' \
         -e '/net.ipv6.conf.default.disable_ipv6/d' \
+        -e '/net.ipv6.conf.lo.disable_ipv6/d' \
         -e '/net.unix.max_dgram_qlen/d' \
         -e '/vm.min_free_kbytes/d' \
         -e '/vm.swappiness/d' \
         -e '/vm.vfs_cache_pressure/d' \
+        -e '/net.ipv4.conf.default.rp_filter/d' \
+        -e '/net.ipv4.conf.all.rp_filter/d' \
+        -e '/net.ipv4.conf.all.accept_source_route/d' \
+        -e '/net.ipv4.conf.default.accept_source_route/d' \
+        -e '/net.ipv4.neigh.default.gc_thresh1/d' \
+        -e '/net.ipv4.neigh.default.gc_thresh2/d' \
+        -e '/net.ipv4.neigh.default.gc_thresh3/d' \
+        -e '/net.ipv4.neigh.default.gc_stale_time/d' \
+        -e '/net.ipv4.conf.default.arp_announce/d' \
+        -e '/net.ipv4.conf.lo.arp_announce/d' \
+        -e '/net.ipv4.conf.all.arp_announce/d' \
+        -e '/kernel.panic/d' \
+        -e '/vm.dirty_ratio/d' \
+        -e '/^#/d' \
+        -e '/^$/d' \
         "$SYS_PATH"
 
 
     ## Add new parameteres. Read More: https://github.com/hawshemi/Linux-Optimizer/blob/main/files/sysctl.conf
 
 cat <<EOF >> "$SYS_PATH"
+
+
+################################################################
+################################################################
+
+
+# /etc/sysctl.conf
+# These parameters in this file will be added/updated to the sysctl.conf file.
+# Read More: https://github.com/hawshemi/Linux-Optimizer/blob/main/files/sysctl.conf
+
+
+## File system settings
+## ----------------------------------------------------------------
+
+# Set the maximum number of open file descriptors
 fs.file-max = 67108864
+
+
+## Network core settings
+## ----------------------------------------------------------------
+
+# Specify default queuing discipline for network devices
 net.core.default_qdisc = fq_codel
+
+# Configure maximum network device backlog
 net.core.netdev_max_backlog = 32768
-net.core.optmem_max = 65536
+
+# Set maximum socket receive buffer
+net.core.optmem_max = 262144
+
+# Define maximum backlog of pending connections
 net.core.somaxconn = 65536
-net.core.rmem_max = 16777216
-net.core.wmem_max = 16777216
+
+# Configure maximum TCP receive buffer size
+net.core.rmem_max = 33554432
+
+# Set default TCP receive buffer size
 net.core.rmem_default = 1048576
+
+# Configure maximum TCP send buffer size
+net.core.wmem_max = 33554432
+
+# Set default TCP send buffer size
 net.core.wmem_default = 1048576
-net.ipv4.tcp_rmem = 8192 1048576 16777216
-net.ipv4.tcp_wmem = 8192 1048576 16777216
+
+
+## TCP settings
+## ----------------------------------------------------------------
+
+# Define socket receive buffer sizes
+net.ipv4.tcp_rmem = 16384 1048576 33554432
+
+# Specify socket send buffer sizes
+net.ipv4.tcp_wmem = 16384 1048576 33554432
+
+# Set TCP congestion control algorithm to BBR
 net.ipv4.tcp_congestion_control = bbr
-net.ipv4.tcp_fastopen = 3
+
+# Configure TCP FIN timeout period
 net.ipv4.tcp_fin_timeout = 25
+
+# Set keepalive time (seconds)
 net.ipv4.tcp_keepalive_time = 1200
+
+# Configure keepalive probes count and interval
 net.ipv4.tcp_keepalive_probes = 7
 net.ipv4.tcp_keepalive_intvl = 30
+
+# Define maximum orphaned TCP sockets
 net.ipv4.tcp_max_orphans = 819200
+
+# Set maximum TCP SYN backlog
 net.ipv4.tcp_max_syn_backlog = 20480
+
+# Configure maximum TCP Time Wait buckets
 net.ipv4.tcp_max_tw_buckets = 1440000
-net.ipv4.tcp_mem = 65536 1048576 16777216
+
+# Define TCP memory limits
+net.ipv4.tcp_mem = 65536 1048576 33554432
+
+# Enable TCP MTU probing
 net.ipv4.tcp_mtu_probing = 1
-net.ipv4.tcp_notsent_lowat = 16384
+
+# Define minimum amount of data in the send buffer before TCP starts sending
+net.ipv4.tcp_notsent_lowat = 32768
+
+# Specify retries for TCP socket to establish connection
 net.ipv4.tcp_retries2 = 8
+
+# Enable TCP SACK and DSACK
 net.ipv4.tcp_sack = 1
 net.ipv4.tcp_dsack = 1
+
+# Disable TCP slow start after idle
 net.ipv4.tcp_slow_start_after_idle = 0
+
+# Enable TCP window scaling
 net.ipv4.tcp_window_scaling = 1
+net.ipv4.tcp_adv_win_scale = -2
+
+# Enable TCP ECN
 net.ipv4.tcp_ecn = 1
-net.ipv4.ip_forward = 1
-net.ipv4.udp_mem = 65536 1048576 16777216
+net.ipv4.tcp_ecn_fallback = 1
+
+# Enable the use of TCP SYN cookies to help protect against SYN flood attacks
+net.ipv4.tcp_syncookies = 1
+
+
+## UDP settings
+## ----------------------------------------------------------------
+
+# Define UDP memory limits
+net.ipv4.udp_mem = 65536 1048576 33554432
+
+
+## IPv6 settings
+## ----------------------------------------------------------------
+
+# Enable IPv6
 net.ipv6.conf.all.disable_ipv6 = 0
-net.ipv6.conf.all.forwarding = 1
+
+# Enable IPv6 by default
 net.ipv6.conf.default.disable_ipv6 = 0
-net.unix.max_dgram_qlen = 50
+
+# Enable IPv6 on the loopback interface (lo)
+net.ipv6.conf.lo.disable_ipv6 = 0
+
+
+## UNIX domain sockets
+## ----------------------------------------------------------------
+
+# Set maximum queue length of UNIX domain sockets
+net.unix.max_dgram_qlen = 256
+
+
+## Virtual memory (VM) settings
+## ----------------------------------------------------------------
+
+# Specify minimum free Kbytes at which VM pressure happens
 vm.min_free_kbytes = 65536
+
+# Define how aggressively swap memory pages are used
 vm.swappiness = 10
-vm.vfs_cache_pressure = 50
+
+# Set the tendency of the kernel to reclaim memory used for caching of directory and inode objects
+vm.vfs_cache_pressure = 250
+
+
+## Network Configuration
+## ----------------------------------------------------------------
+
+# Configure reverse path filtering
+net.ipv4.conf.default.rp_filter = 2
+net.ipv4.conf.all.rp_filter = 2
+
+# Disable source route acceptance
+net.ipv4.conf.all.accept_source_route = 0
+net.ipv4.conf.default.accept_source_route = 0
+
+# Neighbor table settings
+net.ipv4.neigh.default.gc_thresh1 = 512
+net.ipv4.neigh.default.gc_thresh2 = 2048
+net.ipv4.neigh.default.gc_thresh3 = 16384
+net.ipv4.neigh.default.gc_stale_time = 60
+
+# ARP settings
+net.ipv4.conf.default.arp_announce = 2
+net.ipv4.conf.lo.arp_announce = 2
+net.ipv4.conf.all.arp_announce = 2
+
+# Kernel panic timeout
+kernel.panic = 1
+
+# Set dirty page ratio for virtual memory
+vm.dirty_ratio = 20
+
+
+################################################################
+################################################################
+
+
 EOF
-    
-    sleep 0.5
+
     sudo sysctl -p
     
     echo 
     green_msg 'Network is Optimized.'
     echo 
     sleep 0.5
-}
-
-
-# Function to find the SSH port and set it in the SSH_PORT variable
-find_ssh_port() {
-    echo 
-    yellow_msg "Finding SSH port..."
-    echo 
-
-    ## Check if the SSH configuration file exists
-    if [ -e "$SSH_PATH" ]; then
-        ## Use grep to search for the 'Port' directive in the SSH configuration file
-        SSH_PORT=$(grep -oP '^Port\s+\K\d+' "$SSH_PATH" 2>/dev/null)
-
-        if [ -n "$SSH_PORT" ]; then
-            echo 
-            green_msg "SSH port found: $SSH_PORT"
-            echo 
-            sleep 0.5
-        else
-            echo 
-            green_msg "SSH port is default 22."
-            echo 
-            SSH_PORT=22
-            sleep 0.5
-        fi
-    else
-        red_msg "SSH configuration file not found at $SSH_PATH"
-    fi
 }
 
 
